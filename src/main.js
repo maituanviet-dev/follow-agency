53e9676972d4077a914075c927a003361d1baf98
blurk <nsinh6745@gmail.com>
2023-04-06 22:46:43 +0700

    chore: set up base with vuejs, vue-router, tailwind css

---
+++ src/main.js
@@ -0,0 +1,35 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import './style.css'
+import App from './App.vue'
+
+// 1. Define route components.
+// These can be imported from other files
+const About = { template: '<div>About</div>' }
+
+// 2. Define some routes
+// Each route should map to a component.
+// We'll talk about nested routes later.
+const routes = [
+	{ path: '/', component: App },
+	{ path: '/about', component: About }
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = createRouter({
+	// 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+	history: createWebHistory(),
+	routes // short for `routes: routes`
+})
+
+// 5. Create and mount the root instance.
+const app = createApp({})
+// Make sure to _use_ the router instance to make the
+// whole app router-aware.
+app.use(router)
+
+app.mount('#app')
+
+// Now the app has started!
